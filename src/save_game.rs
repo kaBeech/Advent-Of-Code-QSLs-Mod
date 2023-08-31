bae26d2cce51ff9c94de614a62981d74ce528347
kaBeech <contact@kaBeech.com>
2023-08-30 18:30:44 -0700

    Remove Rust components

--- src/save_game.rs
+++
@@ -1,7 +0,0 @@
-use std::fs;
-
-pub(crate) fn save_game(name_of_run: &str, contents: &str) -> std::io::Result<()> {
-    let file_name = name_of_run.to_owned() + ".txt";
-    fs::write(file_name, contents)?;
-    Ok(())
-}
