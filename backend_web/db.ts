cf9edfd2304ff6b20314e07cba919feabba76a30
kaBeech <contact@kaBeech.com>
2023-08-30 20:11:03 -0700

    Abstract Prisma layer

---
+++ backend_web/db.ts
@@ -0,0 +1,74 @@
+import { PrismaClient } from "./generated/client/deno/edge.ts";
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
+import { rollChallengeModifier } from "./components/rollChallengeModifier.ts";
+
+const envVars = await config();
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: envVars.DATABASE_URL,
+    },
+  },
+});
+
+/**
+ * Game CRUD
+ */
+
+export async function getAllGames() {
+  const games = await prisma.game.findMany();
+  return games;
+}
+
+export async function getGameById(id: number) {
+  const game = await prisma.game.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+  return game;
+}
+
+export async function createGame(name: string, playerName: string) {
+  const result = await prisma.game.create({
+    data: {
+      name,
+      player_name: playerName,
+    },
+  });
+  return result;
+}
+
+export async function upsertGame(name: string, playerName: string) {
+  const result = await prisma.game.upsert({
+    where: {
+      name,
+    },
+    update: {},
+    create: {
+      name,
+      player_name: playerName,
+    },
+  });
+  return result;
+}
+
+export async function deleteGame(id: number) {
+  const result = await prisma.game.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  return result;
+}
+
+/**
+ * Challenge Modifier CRUD
+ */
+
+export async function getAllChallengeModifiers() {
+  const challengeModifiers = await prisma.challengeModifier.findMany();
+  return challengeModifiers;
+}
--- backend_web/main.ts
+++ backend_web/main.ts
@@ -1,17 +1,13 @@
-import { PrismaClient } from "./generated/client/deno/edge.ts";
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
 import { rollChallengeModifier } from "./components/rollChallengeModifier.ts";
+import {
+  createGame,
+  deleteGame,
+  getAllChallengeModifiers,
+  getAllGames,
+  getGameById,
+} from "./db.ts";
 
-const envVars = await config();
-
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: envVars.DATABASE_URL,
-    },
-  },
-});
 const app = new Application();
 const router = new Router();
 
@@ -24,47 +20,40 @@ router
       "You have successfully pinged the Advent Of Code QSL's Mod API!";
   })
   /**
-   * Game CRUD
+   * See All Games
    */
-  .get("/game", async (context) => {
-    const games = await prisma.game.findMany();
-    context.response.body = games;
+  .get("/game", (context) => {
+    context.response.body = getAllGames();
   })
-  .get("/game/:id", async (context) => {
+  /**
+   * Resume Game
+   */
+  .get("/game/:id", (context) => {
     const { id } = context.params;
-    const game = await prisma.game.findUnique({
-      where: {
-        id: Number(id),
-      },
-    });
-    context.response.body = game;
+    context.response.body = getGameById(+id);
   })
+  /**
+   * Start New Game
+   */
   .post("/game", async (context) => {
     const { name, playerName } = await context.request.body({ type: "json" })
       .value;
-    const result = await prisma.game.create({
-      data: {
-        name,
-        player_name: playerName,
-      },
-    });
-    context.response.body = result;
-  })
-  .delete("/game/:id", async (context) => {
-    const { id } = context.params;
-    const game = await prisma.game.delete({
-      where: {
-        id: Number(id),
-      },
-    });
-    context.response.body = game;
+    context.response.body = createGame(name, playerName);
   })
   /**
-   * Roll
+   * Delete Game
+   */
+  .delete("/game/:id", (context) => {
+    const { id } = context.params;
+    context.response.body = deleteGame(+id);
+  })
+  /**
+   * Roll a Challenge Modifier
    */
   .get("/roll/challenge_modifier", async (context) => {
-    const challengeModifiers = await prisma.challengeModifier.findMany();
-    context.response.body = rollChallengeModifier(challengeModifiers);
+    context.response.body = rollChallengeModifier(
+      await getAllChallengeModifiers(),
+    );
   });
 
 app.use(router.routes());
