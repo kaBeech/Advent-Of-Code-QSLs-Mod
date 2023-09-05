1b98aaa92577259144d7181dd39da800446ba261
kaBeech <contact@kaBeech.com>
2023-09-05 13:47:53 -0700

    Add more DayController tests

--- backend_web/components/test/DayController_test.ts
+++ backend_web/components/test/DayController_test.ts
@@ -1,6 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.197.0/assert/mod.ts";
 import { DayController } from "../DayController.ts";
 import { exampleDay } from "./exampleObjects.ts";
+import { assertRejects } from "https://deno.land/std@0.197.0/assert/assert_rejects.ts";
 
 const dayController = DayController(exampleDay);
 
@@ -10,6 +11,60 @@ Deno.test("Initial roll returns a ChallengeModifier", async () => {
   assertEquals(typeof result.name, "string");
   assertEquals(typeof result.text, "string");
   assertEquals(typeof result.hasOptions, "boolean");
+});
+
+Deno.test("Initial roll throws error if day is not current", async () => {
+  const dayController = DayController({
+    ...exampleDay,
+    number: 90,
+  });
+  await assertRejects(() => {
+    return dayController.rollInitialChallengeModifier();
+  });
+});
+
+Deno.test("Initial roll throws error if day already has a ChallengeModifier", async () => {
+  const dayController = DayController({
+    ...exampleDay,
+    challengeModifierId: 1,
+  });
+  await assertRejects(
+    () => {
+      return dayController.rollInitialChallengeModifier();
+    },
+  );
+});
+
+Deno.test("Reroll returns a ChallengeModifier", async () => {
+  const result = await dayController.rerollChallengeModifier();
+  assertEquals(typeof result.id, "number");
+  assertEquals(typeof result.name, "string");
+  assertEquals(typeof result.text, "string");
+  assertEquals(typeof result.hasOptions, "boolean");
+});
+
+Deno.test("Reroll throws error if day is not current", async () => {
+  const dayController = DayController({
+    ...exampleDay,
+    number: 90,
+  });
+  await assertRejects(
+    () => {
+      return dayController.rerollChallengeModifier();
+    },
+  );
+});
+
+Deno.test("Reroll throws error if day does not have a ChallengeModifier", async () => {
+  const dayController = DayController({
+    ...exampleDay,
+    challengeModifierId: null,
+  });
+  await assertRejects(
+    () => {
+      return dayController.rerollChallengeModifier();
+    },
+  );
 });
 
 Deno.test("Completes Part 1", async () => {
