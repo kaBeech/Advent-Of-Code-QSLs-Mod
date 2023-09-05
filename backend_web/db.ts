e354f763f63bc9659021713d65e997bff4147733
kaBeech <contact@kaBeech.com>
2023-09-05 16:23:50 -0700

    Cleanup

--- backend_web/db.ts
+++ backend_web/db.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "./generated/client/deno/edge.ts";
 import { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
-import { ChallengeModifier } from "./generated/client/deno/index.d.ts";
 
 const envVars = await config();
 
