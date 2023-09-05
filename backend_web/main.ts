17b961eee3c3d33b176bd383bf4c0e8b16d8dbe9
kaBeech <contact@kaBeech.com>
2023-09-04 21:34:01 -0700

    Add DayController method routes

--- backend_web/main.ts
+++ backend_web/main.ts
@@ -1,6 +1,14 @@
 import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import { rollChallengeModifier } from "./components/rollChallengeModifier.ts";
-import { createGame, deleteGame, getAllGames, getGameById } from "./db.ts";
+import {
+  createGame,
+  deleteGame,
+  getAllGames,
+  getDayById,
+  getDaysByGameId,
+  getGameById,
+} from "./db.ts";
+import { DayController } from "./components/DayController.ts";
 
 const app = new Application();
 const router = new Router();
@@ -14,7 +22,7 @@ router
       "You have successfully pinged the Advent Of Code QSL's Mod API!";
   })
   /**
-   * See All Games (eventually will be Continue Game)
+   * Get All Games (eventually will be Continue Game)
    */
   .get("/game", (context) => {
     context.response.body = getAllGames();
@@ -42,6 +50,51 @@ router
   .delete("/game/:id", (context) => {
     const { id } = context.params;
     context.response.body = deleteGame(+id);
+  })
+  /**
+   * Roll initial Challenge Modifier
+   */
+  .get("day/:id/roll/initial", async (context) => {
+    const { id } = context.params;
+    const day = await getDayById(+id);
+    DayController(day!).rollInitialChallengeModifier();
+    context.response.body = day;
+  })
+  /**
+   * Reroll Challenge Modifier
+   */
+  .get("day/:id/roll/reroll_challenge_modifier", async (context) => {
+    const { id } = context.params;
+    const day = await getDayById(+id);
+    DayController(day!).rerollChallengeModifier();
+    context.response.body = day;
+  })
+  /**
+   * Reroll Modifier Option
+   */
+  .get("day/:id/roll/reroll_modifier_option", async (context) => {
+    const { id } = context.params;
+    const day = await getDayById(+id);
+    DayController(day!).rerollModifierOption();
+    context.response.body = day;
+  })
+  /**
+   * Complete Part 1
+   */
+  .get("day/:id/complete_part_1", async (context) => {
+    const { id } = context.params;
+    const day = await getDayById(+id);
+    DayController(day!).completePart1();
+    context.response.body = day;
+  })
+  /**
+   * Complete Part 2
+   */
+  .get("day/:id/complete_part_2", async (context) => {
+    const { id } = context.params;
+    const day = await getDayById(+id);
+    DayController(day!).completePart2();
+    context.response.body = day;
   })
   /**
    * Roll a Challenge Modifier
