6981315ed30c12a6a722782b03d720ee6c76a4d8
kaBeech <contact@kaBeech.com>
2023-09-05 11:34:09 -0700

    Add exampleObjects

---
+++ backend_web/components/test/exampleObjects.ts
@@ -0,0 +1,25 @@
+export const exampleDay = {
+  "id": 1,
+  "number": 1,
+  "part1Completed": false,
+  "part2Completed": false,
+  "challengeModifierRerollsUsed": 0,
+  "modifierOptionRerollsUsed": 0,
+  "challengeModifierId": null,
+  "modifierOptionId": null,
+  "gameId": 1,
+};
+
+export const exampleGame = {
+  "id": 1,
+  "name": "test game 2",
+  "playerName": "Me!",
+  "year": 1925,
+  "currentDay": 1,
+  "currentDayCompleted": true,
+  "currentRerollTokens": 7,
+  "rerollTokensGained": 7,
+  "rerollTokensSpent": 0,
+  "repositoryLink": null,
+  "progressSheetLink": null,
+};
