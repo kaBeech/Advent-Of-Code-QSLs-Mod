652c3cc912becb99aad5e6a9bd0dbed7fed1646e
kaBeech <contact@kaBeech.com>
2023-09-04 19:20:00 -0700

    Simplify rollChallengeModifier

--- backend_web/components/rollChallengeModifier.ts
+++ backend_web/components/rollChallengeModifier.ts
@@ -1,16 +1,11 @@
-import { ChallengeModifier } from "../generated/client/deno/index.d.ts";
+import { getAllChallengeModifiers } from "../db.ts";
 import { pickRandomly } from "../util/pickRandomly.ts";
 
-const rollChallengeModifier = (challengeModifiers: ChallengeModifier[]) => {
-  const selectedChallengeModifier = pickRandomly(challengeModifiers);
-  let text = selectedChallengeModifier.text;
-  if (selectedChallengeModifier.options.length > 0) {
-    text += `${pickRandomly(selectedChallengeModifier.options)}`;
-  }
-  return {
-    id: selectedChallengeModifier.id,
-    text: `Complete this challenge ${text}!`,
-  };
+const rollChallengeModifier = async () => {
+  const selectedChallengeModifier = pickRandomly(
+    await getAllChallengeModifiers(),
+  );
+  return selectedChallengeModifier;
 };
 
 export { rollChallengeModifier };
