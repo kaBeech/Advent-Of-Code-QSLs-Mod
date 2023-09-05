bfc75854d8ea026f8e2e8f7fd6e6b4e405b08a30
kaBeech <contact@kaBeech.com>
2023-09-05 11:13:14 -0700

    Add rollChallengeModifier_test

---
+++ backend_web/components/test/rollChallengeModifier_test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "https://deno.land/std@0.197.0/assert/mod.ts";
+import { rollChallengeModifier } from "../rollChallengeModifier.ts";
+
+Deno.test(function addTest() {
+  assertEquals(2 + 3, 5);
+});
+
+Deno.test("Rolling returns a ChallengeModifier", async () => {
+  const result = await rollChallengeModifier();
+  assertEquals(typeof result.id, "number");
+  assertEquals(typeof result.name, "string");
+  assertEquals(typeof result.text, "string");
+  assertEquals(typeof result.hasOptions, "boolean");
+});
