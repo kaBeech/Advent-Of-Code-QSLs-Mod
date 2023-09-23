99108cb0c4a9490f183f56fb5021253cc1686c59
kaBeech <contact@kaBeech.com>
2023-09-23 14:20:30 -0700

    Include Day's ModifierOption in user data

--- backend_web/db.ts
+++ backend_web/db.ts
@@ -117,6 +117,7 @@ export async function getUserByIdWithRelations(
                   ModifierOption: true,
                 },
               },
+              ModifierOption: true,
             },
           },
         },
