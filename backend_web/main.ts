1ab7799c30742c6bc6d8a5b45e5b2a3e5267bf39
kaBeech <contact@kaBeech.com>
2023-09-24 17:28:35 -0700

    Add boilerplate OAuth

--- backend_web/main.ts
+++ backend_web/main.ts
@@ -3,6 +3,9 @@ import {
   Context,
   Router,
 } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Session } from "https://deno.land/x/oak_sessions@v4.0.5/mod.ts";
+import { OAuth2Client } from "https://deno.land/x/oauth2_client@v1.0.2/mod.ts";
+
 import {
   createGame,
   createUser,
@@ -22,7 +25,27 @@ import { rollInitialModifier } from "./routes/day/rollInitialModifier.ts";
 import { startNextDay } from "./routes/day/startNextDay.ts";
 import { completeCurrentDay } from "./routes/game/completeCurrentDay.ts";
 
-const app = new Application();
+const githubClientId = "1cfb5aa9850ade3203a3";
+// const githubClientId = Deno.env.get("GITHUB_CLIENT_ID");
+const githubClientSecret = "3b3de07f53954481c2453993a15af07147261214";
+// const githubClientSecret = Deno.env.get("GITHUB_CLIENT_SECRET");
+
+const oauth2Client = new OAuth2Client({
+  clientId: githubClientId,
+  clientSecret: githubClientSecret,
+  authorizationEndpointUri: "https://github.com/login/oauth/authorize",
+  tokenUri: "https://github.com/login/oauth/access_token",
+  redirectUri: "http://127.0.0.1:8000/oauth2/callback",
+  defaults: {
+    scope: "user:read",
+  },
+});
+
+type AppState = {
+  session: Session;
+};
+
+const app = new Application<AppState>();
 const router = new Router();
 
 router
@@ -34,17 +57,40 @@ router
       "You have successfully pinged the Advent Of Code: Xtreme Xmas API!";
   })
   /**
-   * Return User Info (remove eventually)
+   * Login
    */
-  .get(
-    "/privatepage",
-    async (ctx: any, next: any) => {
-      const userId = ctx.locals.id;
-      ctx.response.body = `Welcome, ${userId}!`;
-      const games = await getUserByIdWithRelations(userId);
-      ctx.response.body = games;
-    },
-  )
+  .get("/login", async (ctx) => {
+    // Construct the URL for the authorization redirect and get a PKCE codeVerifier
+    const { uri, codeVerifier } = await oauth2Client.code.getAuthorizationUri();
+
+    // Store both the state and codeVerifier in the user session
+    ctx.state.session.flash("codeVerifier", codeVerifier);
+
+    // Redirect the user to the authorization endpoint
+    ctx.response.redirect(uri);
+  })
+  .get("/oauth2/callback", async (ctx) => {
+    // Make sure the codeVerifier is present for the user's session
+    const codeVerifier = ctx.state.session.get("codeVerifier");
+    if (typeof codeVerifier !== "string") {
+      throw new Error("invalid codeVerifier");
+    }
+
+    // Exchange the authorization code for an access token
+    const tokens = await oauth2Client.code.getToken(ctx.request.url, {
+      codeVerifier,
+    });
+
+    // Use the access token to make an authenticated API request
+    const userResponse = await fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${tokens.accessToken}`,
+      },
+    });
+    const { login } = await userResponse.json();
+
+    ctx.response.body = `Hello, ${login}!`;
+  })
   /**
    * Log Out
    */
@@ -221,6 +267,7 @@ router
     },
   );
 
+app.use(Session.initMiddleware());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
