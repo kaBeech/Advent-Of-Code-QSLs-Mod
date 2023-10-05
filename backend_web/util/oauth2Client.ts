56643bb78ea0492c801647f7c29140726a3f5869
kaBeech <contact@kaBeech.com>
2023-10-05 15:03:46 -0700

    Fix dotEnv

--- backend_web/util/oauth2Client.ts
+++ backend_web/util/oauth2Client.ts
@@ -1,13 +1,11 @@
 import { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
 import { OAuth2Client } from "https://deno.land/x/oauth2_client@v1.0.2/mod.ts";
 
-const _dotEnv = await config();
+const dotEnv = await config();
 
-const githubClientId = "GITHUB_CLIENT_ID";
-const githubClientSecret = "GITHUB_CLIENT_SECRET";
 export const oauth2Client = new OAuth2Client({
-  clientId: githubClientId!,
-  clientSecret: githubClientSecret!,
+  clientId: dotEnv.GITHUB_CLIENT_ID,
+  clientSecret: dotEnv.GITHUB_CLIENT_SECRET,
   authorizationEndpointUri: "https://github.com/login/oauth/authorize",
   tokenUri: "https://github.com/login/oauth/access_token",
   redirectUri: "http://127.0.0.1:8000/oauth2/callback",
