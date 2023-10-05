8c8c49fbd12d0f492d9fc2701d519355bfe6a51f
kaBeech <contact@kaBeech.com>
2023-10-04 18:33:50 -0700

    Modularize serverFetcher

--- frontend_web/src/routes/login/index.tsx
+++ frontend_web/src/routes/login/index.tsx
@@ -1,40 +1,8 @@
 // import type { Session } from "@auth/core/types";
 import { $, component$, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { Link, server$ } from "@builder.io/qwik-city";
-import { getFormData } from "~/util/getFormData";
-
-export const serverFetcher = server$(async function (
-  route: string,
-  method: string,
-  username: string,
-  password: string
-) {
-  //   const xtremeXmasAPI = this.env.get("XTREME_XMAS_API");
-  // if (xtremeXmasAPI == undefined) {
-  //   console.error("XTREME_XMAS_API string not found upon request");
-  // }
-  const xtremeXmasAPI = "http://localhost:8000";
-  const abortController = new AbortController();
-  if (method !== `GET`) {
-    const bodyData = { username, password };
-    const bodyFormData = getFormData(bodyData);
-    const res = await fetch(`${xtremeXmasAPI}/${route}`, {
-      signal: abortController.signal,
-      method,
-      body: bodyFormData,
-    });
-    const data = await res.json();
-    return data;
-  } else {
-    const res = await fetch(`${xtremeXmasAPI}/${route}`, {
-      signal: abortController.signal,
-      method,
-    });
-    const data = await res.json();
-    return data;
-  }
-});
+import { Link } from "@builder.io/qwik-city";
+import { serverFetcher } from "~/util/serverFetcher";
 
 export default component$(() => {
   const state = useStore({
---
+++ frontend_web/src/util/serverFetcher.ts
@@ -0,0 +1,34 @@
+import { server$ } from "@builder.io/qwik-city";
+import { getFormData } from "./getFormData";
+
+export const serverFetcher = server$(async function (
+  route: string,
+  method: string,
+  username: string,
+  password: string,
+) {
+  //   const xtremeXmasAPI = this.env.get("XTREME_XMAS_API");
+  // if (xtremeXmasAPI == undefined) {
+  //   console.error("XTREME_XMAS_API string not found upon request");
+  // }
+  const xtremeXmasAPI = "http://localhost:8000";
+  const abortController = new AbortController();
+  if (method !== `GET`) {
+    const bodyData = { username, password };
+    const bodyFormData = getFormData(bodyData);
+    const res = await fetch(`${xtremeXmasAPI}/${route}`, {
+      signal: abortController.signal,
+      method,
+      body: bodyFormData,
+    });
+    const data = await res.json();
+    return data;
+  } else {
+    const res = await fetch(`${xtremeXmasAPI}/${route}`, {
+      signal: abortController.signal,
+      method,
+    });
+    const data = await res.json();
+    return data;
+  }
+});
