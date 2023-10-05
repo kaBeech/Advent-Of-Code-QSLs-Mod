aa8784dc41183c0c8d1768c51968663144c8120e
kaBeech <contact@kaBeech.com>
2023-10-05 15:08:49 -0700

    Add local storage and testing buttons

--- frontend_web/src/routes/login/index.tsx
+++ frontend_web/src/routes/login/index.tsx
@@ -1,10 +1,22 @@
 // import type { Session } from "@auth/core/types";
-import { $, component$, useStore } from "@builder.io/qwik";
+import {
+  $,
+  component$,
+  useSignal,
+  useStore,
+  useVisibleTask$,
+} from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Link } from "@builder.io/qwik-city";
 import { serverFetcher } from "~/util/serverFetcher";
 
 export default component$(() => {
+  const ref = useSignal<Element>();
+
+  useVisibleTask$(() => {
+    ref.value!.innerHTML = "Changed!";
+  });
+
   const state = useStore({
     username: "USERNAME",
     password: "PASSWORD",
@@ -18,7 +30,36 @@ export default component$(() => {
       state.username,
       state.password
     );
-    console.log(data);
+    localStorage.setItem("token", data.token);
+    sessionStorage.setItem("token", data.token);
+    state.buttonPresses++;
+  });
+
+  const getData = $(async () => {
+    const data = await fetch(`http://127.0.0.1:8000/userdata`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    const json = await data.json();
+    console.log(json);
+    state.buttonPresses++;
+  });
+
+  const loginGitHub = $(async () => {
+    const data = await fetch(`http://127.0.0.1:8000/log-in/github`, {
+      method: "GET",
+    });
+    const json = await data.json();
+    const uri = json.uri;
+    const githubOAuth = await fetch(uri, {
+      method: "GET",
+    });
+    const githubOAuthJson = await githubOAuth.json();
+    console.debug(githubOAuthJson);
+    // localStorage.setItem("token", data.token);
+    // sessionStorage.setItem("token", data.token);
     state.buttonPresses++;
   });
 
@@ -41,7 +82,18 @@ export default component$(() => {
           value={"PASSWORD"}
           aria-labelledby="Day ID"
         />
-        <button onClick$={login}>TEST</button>
+        <button onClick$={login}>LOGIN</button>
+        <button
+          ref={ref}
+          onClick$={() => {
+            ref.value!.innerHTML = "Clicked!";
+            document.title = "Hellooooo world!!";
+          }}
+        >
+          Click me
+        </button>
+        <button onClick$={loginGitHub}>Login with GitHub</button>
+        <button onClick$={getData}>GET DATA</button>
 
         <p>
           <Link href="../">{"<-- Back"}</Link>
