bdbf17e16f6ceb6555c1cf63f35f22b202e96b97
kaBeech <contact@kaBeech.com>
2023-10-06 00:57:49 -0700

    Add prelim revised authenticate function

--- backend_web/middleware/authenticate.ts
+++ backend_web/middleware/authenticate.ts
@@ -1,6 +1,9 @@
 import { verify } from "https://deno.land/x/djwt@v2.9.1/mod.ts";
 import { key } from "../util/apiKey.ts";
 import { Context, Next } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { config } from "https://deno.land/std@0.163.0/dotenv/mod.ts";
+
+const dotEnv = await config();
 
 export const authenticate = async (ctx: Context, next: Next) => {
   try {
@@ -21,6 +24,38 @@ export const authenticate = async (ctx: Context, next: Next) => {
       throw new Error("!payload");
     }
     ctx.state.session.set("userId", payload.payload.id);
+    await next();
+  } catch (_error) {
+    ctx.response.status = 401;
+    ctx.response.body = {
+      message: "You are not authorized to access this route",
+    };
+    return;
+  }
+};
+
+export const authenticateNew = async (ctx: Context, next: Next) => {
+  try {
+    const headers: Headers = ctx.request.headers;
+    const authorization = headers.get("Authorization");
+    if (!authorization) {
+      ctx.response.status = 401;
+      return;
+    }
+    const xmasSecret = authorization.split(" ")[1];
+
+    if (!xmasSecret) {
+      ctx.response.status = 401;
+      return;
+    }
+    // const payload = await verify(jwt, key);
+    if (xmasSecret !== dotEnv.XMAS_SECRET) {
+      throw new Error("XMAS_SECRET does not match");
+    }
+    const body = await ctx.request.body().value;
+    const json = await body.json();
+    console.log(json);
+    ctx.state.session.set("userId", json.userId);
     await next();
   } catch (_error) {
     ctx.response.status = 401;
