64ba1cb290575751aef308f57d181b1a8def5643
kaBeech <contact@kaBeech.com>
2023-10-10 13:58:33 -0700

    Update heading text

--- frontend_web/src/components/header/header.tsx
+++ frontend_web/src/components/header/header.tsx
@@ -11,12 +11,12 @@ export default component$(() => {
         <div class="flex column">
           <div class="flex">
             {" "}
-            <div>[About]</div> <div>[Events]</div> <div>[Shop]</div>{" "}
+            <div>[About]</div> <div>[Games]</div> <div>[Shop???]</div>{" "}
             <div>[Settings]</div> <div>[Log Out]</div> <div>NAME</div>{" "}
           </div>
           <div class="flex">
             {" "}
-            <div>[Calendar]</div> <div>[XX++]</div> <div>[Sponsors]</div>{" "}
+            <div>[Progress]</div> <div>[XX++]</div> <div>[Sponsors]</div>{" "}
             <div>[Leaderboard]</div> <div>[Stats]</div>
           </div>
         </div>
