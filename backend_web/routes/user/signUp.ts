a491e38db37c703da09a0ea292878a26c0629ad8
kaBeech <contact@kaBeech.com>
2023-10-11 00:21:52 -0700

    Create user upon signup if not yet extant

--- backend_web/routes/user/signUp.ts
+++ backend_web/routes/user/signUp.ts
@@ -1,10 +1,15 @@
-import { createUser } from "../../db.ts";
+import { createUser, getUserById } from "../../db.ts";
 import { Context, Next } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 export const signUp = async (ctx: Context, next: Next) => {
   try {
     const userId = ctx.state.session.get("userId") as string;
-    const user = await createUser(userId);
+    let user;
+    try {
+      user = await getUserById(userId);
+    } catch (_err) {
+      user = await createUser(userId);
+    }
     ctx.response.status = 201;
     ctx.response.body = {
       message: "User created",
