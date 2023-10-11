0fe568908691c91236c5da282674e97d8345fe73
kaBeech <contact@kaBeech.com>
2023-10-11 03:46:14 -0700

    Cleanup

--- frontend_web/src/components/signIn/signIn.tsx
+++ frontend_web/src/components/signIn/signIn.tsx
@@ -1,18 +1,13 @@
 import { component$ } from "@builder.io/qwik";
 import { useAuthSignin } from "~/routes/plugin@auth";
 
-// If we can guarantee that the user is redirected to the /test page,
-// then auth works great
-
 export default component$(() => {
   const signIn = useAuthSignin();
   return (
     <a
-      // href="/test"
       onClick$={() => {
         signIn.submit({
           providerId: "github",
-          // options: { callbackUrl: "test" },
         });
       }}
     >
