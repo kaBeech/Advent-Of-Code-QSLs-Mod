ec25ca1e130be1f665bcd43b4b3b279270bbb56e
kaBeech <contact@kaBeech.com>
2023-10-10 21:51:41 -0700

    Process FormData

--- backend_web/routes/game/startNewGame.ts
+++ backend_web/routes/game/startNewGame.ts
@@ -1,21 +1,21 @@
-import { Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import {
+  Context,
+  FormDataReader,
+} from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import { createGame, getUserById, updateUser } from "../../db.ts";
 
 export const startNewGame = async (ctx: Context) => {
   const userId = ctx.state.session.get("userId") as string;
   const user = await getUserById(userId);
-  const { name, year, playerName } = await ctx.request
-    .body({
-      type: "json",
-    })
-    .value;
+  const body: FormDataReader = ctx.request.body({ type: "form-data" }).value;
+  const bodyData = await body.read();
   user!.numberOfGames++;
   await updateUser(user!);
   ctx.response.body = await createGame(
     userId,
     user!.numberOfGames,
-    name,
-    year,
-    playerName,
+    bodyData.fields.name,
+    +bodyData.fields.year,
+    bodyData.fields.playerName,
   );
 };
