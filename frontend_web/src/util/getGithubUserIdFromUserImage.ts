20f5087880e8a6a8ed4b50fc00f82c9f437da17a
kaBeech <contact@kaBeech.com>
2023-10-10 18:30:49 -0700

    Temporarily generalize GitHub userId

--- frontend_web/src/util/getGithubUserIdFromUserImage.ts
+++ frontend_web/src/util/getGithubUserIdFromUserImage.ts
@@ -2,6 +2,7 @@ export const getGithubUserIdFromUserImage = (userImage: string) => {
   const rawRegex = /u\/[0-9]+/g;
   const userIdRegex = /[0-9]+/g;
   const rawUserId = userImage.match(rawRegex);
-  const userId = "github" + rawUserId![0].match(userIdRegex)![0];
+  // const userId = "github" + rawUserId![0].match(userIdRegex)![0];
+  const userId = rawUserId![0].match(userIdRegex)![0];
   return userId;
 };
