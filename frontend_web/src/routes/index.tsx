f3290312d7e1760d0bc0eea6bd1b9bc18f0f2bf6
kaBeech <contact@kaBeech.com>
2023-10-11 15:34:28 -0700

    Remove redundant user creation

--- frontend_web/src/routes/index.tsx
+++ frontend_web/src/routes/index.tsx
@@ -1,16 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
-import { getGithubUserIdFromUserImage } from "~/util/getGithubUserIdFromUserImage";
-import { useAuthSession } from "./plugin@auth";
-import { serverFetcher } from "~/util/serverFetcher";
 
 export default component$(() => {
-  const session = useAuthSession();
-  // This is to create a user in the database after signing in for the first time
-  if (session.value) {
-    const userId = getGithubUserIdFromUserImage(session.value!.user!.image!);
-    serverFetcher(`user`, "PUT", userId);
-  }
   return (
     <>
       <div role="presentation" class="ellipsis"></div>
