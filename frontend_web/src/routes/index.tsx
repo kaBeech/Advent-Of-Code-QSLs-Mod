4f694d0681478c05c935567c66543334a3cb2885
kaBeech <contact@kaBeech.com>
2023-10-11 18:23:47 -0700

    Redirect home page to game page

--- frontend_web/src/routes/index.tsx
+++ frontend_web/src/routes/index.tsx
@@ -1,5 +1,9 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
+
+export const onRequest: RequestHandler = (event) => {
+  throw event.redirect(302, `/game`);
+};
 
 export default component$(() => {
   return (
