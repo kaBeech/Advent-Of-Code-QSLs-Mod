b163ea50e32d61b3dbaded5de469ddab3f9486ff
kaBeech <contact@kaBeech.com>
2023-10-26 15:21:18 -0700

    Add unique repick option

--- backend_web/util/pickRandomly.ts
+++ backend_web/util/pickRandomly.ts
@@ -1,7 +1,19 @@
 // deno-lint-ignore no-explicit-any
-const pickRandomly = (selectFrom: any[]) => {
+type SelectionType = any;
+
+const pickRandomly = (
+  selectFrom: SelectionType[],
+  // Only add currentSelection if you're re-picking and don't want to pick the same thing again
+  currentSelection?: SelectionType,
+): SelectionType => {
   const randomIndex = Math.floor(Math.random() * selectFrom.length);
-  return selectFrom[randomIndex];
+  const randomSelection = selectFrom[randomIndex];
+
+  if (randomSelection === currentSelection) {
+    return pickRandomly(selectFrom, currentSelection);
+  }
+
+  return randomSelection;
 };
 
 export { pickRandomly };
