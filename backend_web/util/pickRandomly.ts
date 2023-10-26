c2d409676a6fe89b86762634fa473df0c3a1196f
kaBeech <contact@kaBeech.com>
2023-10-26 15:38:35 -0700

    Handle error for no items on input

--- backend_web/util/pickRandomly.ts
+++ backend_web/util/pickRandomly.ts
@@ -6,6 +6,10 @@ const pickRandomly = (
   // Only add currentSelection if you're re-picking and don't want to pick the same thing again
   currentSelection?: SelectionType,
 ): SelectionType => {
+  if (selectFrom.length === 0) {
+    throw new Error("No items to pick from");
+  }
+
   const randomIndex = Math.floor(Math.random() * selectFrom.length);
   const randomSelection = selectFrom[randomIndex];
 
