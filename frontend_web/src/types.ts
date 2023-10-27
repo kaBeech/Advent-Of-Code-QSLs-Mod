a6e538260b79428644c6f6da0950ab465fac083a
kaBeech <contact@kaBeech.com>
2023-10-26 19:24:20 -0700

    Add types.ts

---
+++ frontend_web/src/types.ts
@@ -0,0 +1,23 @@
+export interface GameInfo {
+  numberOfGames: string;
+  challengeModifier: string;
+  modifierOption: string;
+  rerollTokensSpentDuringPart1: number;
+  rerollTokensSpentDuringPart2: number;
+  currentRerollTokens: number;
+  netScore: number;
+  currentDay: number;
+  currentDayCompleted: boolean;
+  part1Completed: Date | null;
+  modifierWhenPart1Completed: string;
+  optionWhenPart1Completed: string;
+  part2Completed: Date | null;
+}
+
+export interface UserData {
+  Game: {
+    name: string;
+    number: number;
+    year: number;
+  }[];
+}
