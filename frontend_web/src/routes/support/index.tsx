d3c943ebe56a12e3290794898e21cf796cae4797
kaBeech <contact@kaBeech.com>
2023-11-07 14:14:58 -0800

    Clarify Support text

--- frontend_web/src/routes/support/index.tsx
+++ frontend_web/src/routes/support/index.tsx
@@ -6,7 +6,7 @@ export default component$(() => {
       <h1>Support</h1>
       <p>
         If you'd like to support Xtreme Xmas Code, you can do so by sharing it
-        with a friend, supporting our{" "}
+        with a friend, supporting XXC's{" "}
         <a href="/sponsors" class="link">
           °sponsors°
         </a>
@@ -14,18 +14,22 @@ export default component$(() => {
         <a href="https://adventofcode.com/support" class="link">
           °Eric Wastl°
         </a>
-        , creator of Advent of Code.
+        , creator of the original Advent of Code.
       </p>
       <p>
-        I'm also <em>#OpenToWork</em> and currently looking to move into a
-        full-time software engineering role (right now I'm daylighting in
-        healthcare and freelancing in web development). If you're looking to
-        work with a fun nerd you may contact me via{" "}
+        I (<a href="/about#aboutMe">°Kyle Beechly°</a>) am also{" "}
+        <em>#OpenToWork</em> and currently looking to move into a full-time
+        software engineering role (right now I'm daylighting in healthcare and
+        freelancing in web development). If you're looking to work with a fun
+        nerd you may contact me via{" "}
         <a href="https://www.linkedin.com/in/kyle-beechly/" class="link">
           °LinkedIn°
         </a>
         ,{" "}
-        <a href="mailto:contact@kabeech.com" class="link">
+        <a
+          href="mailto:contact@kabeech.com?subject=Let%27s%20work%20together%21"
+          class="link"
+        >
           °Email°
         </a>
         ,{" "}
