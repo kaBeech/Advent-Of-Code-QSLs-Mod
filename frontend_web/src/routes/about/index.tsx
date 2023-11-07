d5657a603aa4588d604d068a713284dca440021a
kaBeech <contact@kaBeech.com>
2023-11-07 14:35:32 -0800

    Slight text adjustment

--- frontend_web/src/routes/about/index.tsx
+++ frontend_web/src/routes/about/index.tsx
@@ -87,15 +87,16 @@ export default component$(() => {
         </p>
         <p>
           If you'd like to support Xtreme Xmas Code, you can do so by sharing it
-          with a friend, supporting our{" "}
+          with a friend, supporting XXC's{" "}
           <a href="/sponsors" class="link">
             °sponsors°
-          </a>{" "}
+          </a>
           , or supporting{" "}
           <a href="https://adventofcode.com/support" class="link">
             °Eric Wastl°
           </a>
-          , creator of Advent of Code. You can find more information on{" "}
+          , creator of the original Advent of Code. You can find more
+          information on{" "}
           <a href="/support" class="link">
             °my support page°
           </a>
