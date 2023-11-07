27f717dc19a428cf1ed7f6352977ceedcb7d0cb4
kaBeech <contact@kaBeech.com>
2023-11-06 19:33:26 -0800

    Add removeChallengeModifier route

---
+++ backend_web/routes/day/removeChallengeModifier.ts
@@ -0,0 +1,25 @@
+import { State } from "https://deno.land/x/oak@v12.6.1/application.ts";
+import { RouterContext } from "https://deno.land/x/oak@v12.6.1/router.ts";
+import { DayController } from "../../components/DayController.ts";
+import { getUserByIdWithRelations, updateDay } from "../../db.ts";
+
+export const removeChallengeModifier = async (
+  ctx: RouterContext<
+    "/game/:gameNumber/day/:dayNumber/removeChallengeModifier",
+    {
+      gameNumber: string;
+    } & {
+      dayNumber: string;
+    } & Record<string | number, string | undefined>,
+    State
+  >,
+) => {
+  const { gameNumber, dayNumber } = ctx.params;
+  const userId = ctx.state.session.get("userId") as string;
+  const userData = await getUserByIdWithRelations(userId);
+  const game = userData.Game.find((game) => game.number === +gameNumber);
+  const day = game!.Day.find((day) => day.number === +dayNumber);
+  const updatedDay = DayController(day!).removeChallengeModifier(game!);
+  await updateDay(updatedDay);
+  ctx.response.body = updatedDay;
+};
