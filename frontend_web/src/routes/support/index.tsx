e7c19d9074b28a741aa272e5b8ac4f6ee7dad2dd
kaBeech <contact@kaBeech.com>
2023-11-06 21:48:52 -0800

    Add Support info

--- frontend_web/src/routes/support/index.tsx
+++ frontend_web/src/routes/support/index.tsx
@@ -3,9 +3,40 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <article>
-      <h1>Support</h1>
+      <h1 class="visualHide">Support</h1>
       <p>
-        <em>TBA!</em>
+        If you'd like to support Xtreme Xmas Code, you can do so by sharing it
+        with a friend, supporting our{" "}
+        <a href="/support" class="link">
+          °sponsors°
+        </a>
+        , or supporting{" "}
+        <a href="https://adventofcode.com/support" class="link">
+          °Eric Wastl°
+        </a>
+        , creator of Advent of Code.
+      </p>
+      <p>
+        I'm also currently looking to move into a full-time software engineering
+        role (right now I'm daylighting in healthcare and freelancing in web
+        development), so if you're looking to work with a fun nerd you may
+        contact me via{" "}
+        <a href="https://www.linkedin.com/in/kyle-beechly/" class="link">
+          °LinkedIn°
+        </a>
+        ,{" "}
+        <a href="mailto:contact@kabeech.com" class="link">
+          °Email°
+        </a>
+        ,{" "}
+        <a href="https://bsky.app/profile/kabeech.com" class="link">
+          °Bluesky°
+        </a>
+        , or{" "}
+        <a href="https://hachyderm.io/@kaBeech" class="link">
+          °Mastodon°
+        </a>
+        !
       </p>
     </article>
   );
