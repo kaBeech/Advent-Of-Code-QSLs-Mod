a669cd0d02d68b5486e59c12c6afcfa9146476b0
kaBeech <contact@kaBeech.com>
2023-11-29 22:46:50 -0800

    Add Thomas as a sponsor

--- frontend_web/src/data/sponsors.ts
+++ frontend_web/src/data/sponsors.ts
@@ -14,4 +14,11 @@ export default [
     textLine1: `kaBeech.com is also owned by me (the creator of this website).`,
     textLine2: `Check it out if you want to see some more cool stuff I made!`,
   },
+  {
+    name: `Thomas Johnston`,
+    url: `https://thomasjohnston.services/`,
+    textLine1:
+      `I'm a software consultant specializing in full-stack web development and DevOps.`,
+    textLine2: `Can I help you build something?`,
+  },
 ];
