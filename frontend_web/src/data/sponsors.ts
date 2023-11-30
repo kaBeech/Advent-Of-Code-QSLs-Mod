55dac8d4e0a6bf548ced671c43fa1cd208b1e21b
kaBeech <contact@kaBeech.com>
2023-11-30 05:06:59 -0800

    Clarify sponsor text

--- frontend_web/src/data/sponsors.ts
+++ frontend_web/src/data/sponsors.ts
@@ -5,13 +5,14 @@ export default [
     textLine1: `I'm the creator of Xtreme Xmas
         Code; I hope you like it!`,
     textLine2:
-      `Please reach out if you have any questions/comments or if you'd like to
+      `Feel free to reach out with any questions/comments or if you'd like to
         work together!`,
   },
   {
     name: `kaBeech.com`,
     url: `https://www.kabeech.com/`,
-    textLine1: `kaBeech.com is also owned by me (the creator of this website).`,
+    textLine1:
+      `kaBeech.com is also made by me (Kyle, the creator of Xtreme Xmas Code).`,
     textLine2: `Check it out if you want to see some more cool stuff I made!`,
   },
   {
