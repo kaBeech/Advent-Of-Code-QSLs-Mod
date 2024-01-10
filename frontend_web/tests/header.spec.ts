979357d0d1b407ca322c5ab30645485ca3b68868
kaBeech <contact@kaBeech.com>
2024-01-10 15:25:17 -0800

    Try beforeEach to fix test fail

--- frontend_web/tests/header.spec.ts
+++ frontend_web/tests/header.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("navigation", () => {
-  test.beforeAll(async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     // Go to the starting url before each test.
     await page.goto("/about");
   });
