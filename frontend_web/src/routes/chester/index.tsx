4e9f94617274e93cbeb5b4cacf8297aae176b58e
kaBeech <contact@kaBeech.com>
2024-01-10 21:31:45 -0800

    Add testing login

---
+++ frontend_web/src/routes/chester/index.tsx
@@ -0,0 +1,48 @@
+import type { Session } from "@auth/core/types";
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead, RequestHandler } from "@builder.io/qwik-city";
+import { useAuthSignin } from "../plugin@auth";
+
+export const onRequest: RequestHandler = (event) => {
+  const session: Session | null = event.sharedMap.get("session");
+  if (session && new Date(session.expires) > new Date()) {
+    throw event.redirect(302, `/game/`);
+  }
+};
+
+export default component$(() => {
+  const logIn = useAuthSignin();
+
+  return (
+    <>
+      <h1 class="logo stitch logoWhite">
+        {"<"}
+        <span class="logoRed"> Welcome! </span>
+        {"<"}
+      </h1>
+      <p class="textCenter fontLarger marginTop0">
+        Please log in with{" "}
+        <a
+          onClick$={() => {
+            logIn.submit({ providerId: "credentials" });
+          }}
+          class="textGreen"
+        >
+          °Chester The Tester°
+        </a>{" "}
+        to play!
+      </p>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Xtreme Xmas Code - Cheseter The Tester",
+  meta: [
+    {
+      name: "description",
+      content:
+        "Xtreme Xmas Code - an invigorating twist on your favorite advent calendar",
+    },
+  ],
+};
