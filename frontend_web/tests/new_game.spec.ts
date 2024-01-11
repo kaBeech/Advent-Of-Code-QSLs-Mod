5afd9cfe37243c76c5df08db79b2e62f96cf989e
kaBeech <contact@kaBeech.com>
2024-01-10 23:35:29 -0800

    Use getByRole()

--- frontend_web/tests/new_game.spec.ts
+++ frontend_web/tests/new_game.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from "@playwright/test";
 test.describe("new game", () => {
   test("day 1 happy path", async ({ page }) => {
     await page.goto("chester");
-    await page.getByText("°Chester The Tester°").click();
+    await page.getByRole("link", { name: "°Chester The Tester°" }).click();
     await page.getByRole("link", { name: "°Games°" }).click();
     await page.getByRole("link", { name: "°New Game°" }).click();
     await page.locator("#name").click();
@@ -19,18 +19,21 @@ test.describe("new game", () => {
     await page.locator("#year").click();
     await page.locator("#year").fill("2014");
     await page.locator("#year").press("End");
-    await page.getByText("°Create New Game°").click();
+    await page.getByRole("link", { name: "°Create New Game°" }).click();
     await page.getByRole("link", { name: "°Continue Current Day°" }).click();
     await page.getByRole("link", { name: "°Roll Initial Challenge Modifier°" })
       .click();
-    await page.getByText("°Reroll Challenge Modifier°").click();
-    await page.getByText("°Remove Challenge Modifier°").click();
-    await page.getByText("°Reroll Challenge Modifier°").click();
+    await page.getByRole("link", { name: "°Reroll Challenge Modifier°" })
+      .click();
+    await page.getByRole("link", { name: "°Remove Challenge Modifier°" })
+      .click();
+    await page.getByRole("link", { name: "°Reroll Challenge Modifier°" })
+      .click();
     await page.getByRole("link", { name: "°Complete Part 1°" }).click();
-    await page.getByText("°Remove Challenge Modifier°").click();
-    await page.getByText("°Reroll Challenge Modifier°").click();
+    await page.getByRole("link", { name: "°Remove Challenge Modifier°" });
+    await page.getByRole("link", { name: "°Reroll Challenge Modifier°" });
     await page.getByRole("link", { name: "°Complete Part 2°" }).click();
-    await page.getByText("°Start Next Day°").click();
+    await page.getByRole("link", { name: "°Start Next Day°" }).click();
     await page.getByText("20").click();
     await page.getByRole("link", { name: "°Previous Day°" }).click();
     await page.getByText("20").click();
