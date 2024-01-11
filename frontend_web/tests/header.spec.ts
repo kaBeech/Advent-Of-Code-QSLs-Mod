5810c0f37d4899b5bcde71501f7579adb6f6fe94
kaBeech <contact@kaBeech.com>
2024-01-10 16:09:16 -0800

    Simplify header boilerplate

--- frontend_web/tests/header.spec.ts
+++ frontend_web/tests/header.spec.ts
@@ -1,21 +1,13 @@
 import { expect, test } from "@playwright/test";
 
-test.describe("navigation", () => {
+test.describe("Header", () => {
   test.beforeEach(async ({ page }) => {
-    // Go to the starting url before each test.
     await page.goto("/about");
   });
 
-  test("About page has title", async ({ page }) => {
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Xtreme Xmas Code - About/);
-  });
-
-  test("About page links to homepage", async ({ page }) => {
-    // create a locator
+  test("Logo links to homepage", async ({ page }) => {
     const anchor = page.locator("a.logo");
 
-    // Expect an attribute "to be strictly equal" to the value.
     await expect(anchor).toHaveAttribute("href", "/");
   });
 });
