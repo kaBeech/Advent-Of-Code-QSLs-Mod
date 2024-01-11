b28a88f49f08254ef2f4d0e2970e22a4d33ee0ca
kaBeech <contact@kaBeech.com>
2024-01-10 16:07:31 -0800

    Update example test

--- frontend_web/tests/example.spec.ts
+++ frontend_web/tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test.describe("example1", () => {
+test.describe("example", () => {
   test.beforeEach(async ({ page }) => {
     // Go to the starting url before each test.
     await page.goto("/about");
@@ -11,27 +11,12 @@ test.describe("example1", () => {
     await expect(page).toHaveTitle(/Xtreme Xmas Code - About/);
   });
 
-  test("About page links to homepage", async ({ page }) => {
-    // create a locator
-    const anchor = page.locator("a.logo");
-
-    // Expect an attribute "to be strictly equal" to the value.
-    await expect(anchor).toHaveAttribute("href", "/");
-  });
-});
-
-test.describe("example2", () => {
-  test.beforeEach(async ({ page }) => {
-    // Go to the starting url before each test.
-    await page.goto("/sponsors");
-  });
-
   test("Sponsors page has title", async ({ page }) => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Xtreme Xmas Code - Sponsors/);
   });
 
-  test("About page links to homepage", async ({ page }) => {
+  test("Logo links to homepage", async ({ page }) => {
     // create a locator
     const anchor = page.locator("a.logo");
 
