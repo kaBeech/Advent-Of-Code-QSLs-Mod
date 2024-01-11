9c43ce09f10e6128837ce46d1c992d2014d50144
kaBeech <contact@kaBeech.com>
2024-01-10 20:05:45 -0800

    Add auth to a11y checks

--- frontend_web/tests/basic_a11y.spec.ts
+++ frontend_web/tests/basic_a11y.spec.ts
@@ -11,39 +11,9 @@ test.describe("about page", () => {
   });
 });
 
-test.describe("calendar page", () => {
-  test("does not have any automatically detectable accessibility issues", async ({ page }) => {
-    await page.goto("calendar");
-
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
-});
-
 test.describe("modifiers page", () => {
   test("does not have any automatically detectable accessibility issues", async ({ page }) => {
     await page.goto("modifier");
-
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
-});
-
-test.describe("settings page", () => {
-  test("does not have any automatically detectable accessibility issues", async ({ page }) => {
-    await page.goto("settings");
-
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
-});
-
-test.describe("games page", () => {
-  test("does not have any automatically detectable accessibility issues", async ({ page }) => {
-    await page.goto("games");
 
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
@@ -88,5 +58,53 @@ test.describe("log in page", () => {
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
 
     expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe("pages needing auth", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://www.xtremexmascode.com/login/");
+    await page.getByRole("main").getByText("°Log In°").click();
+    await page.getByRole("button", { name: "Sign in with Reddit" }).click();
+    await page.getByPlaceholder("\n        Username\n      ").click();
+    await page.getByPlaceholder("\n        Username\n      ").fill(
+      process.env.TEST_USER_REDDIT!,
+    );
+    await page.getByPlaceholder("\n        Password\n      ").click();
+    await page.getByPlaceholder("\n        Password\n      ").fill(
+      process.env.TEST_PASSWORD_REDDIT!,
+    );
+    await page.getByRole("button", { name: "Log In" }).click();
+    await page.getByRole("button", { name: "Allow" }).click();
+  });
+
+  test.describe("calendar page", () => {
+    test("does not have any automatically detectable accessibility issues", async ({ page }) => {
+      await page.goto("calendar");
+
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  test.describe("settings page", () => {
+    test("does not have any automatically detectable accessibility issues", async ({ page }) => {
+      await page.goto("settings");
+
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+
+  test.describe("games page", () => {
+    test("does not have any automatically detectable accessibility issues", async ({ page }) => {
+      await page.goto("games");
+
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
   });
 });
