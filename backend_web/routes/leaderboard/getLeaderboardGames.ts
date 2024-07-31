44c9e9272554ae017cd4e46b55fc236ec9153a73
kaBeech <contact@kaBeech.com>
2024-07-31 16:54:45 -0700

    Update naming

--- backend_web/routes/leaderboard/getLeaderboardGames.ts
+++ backend_web/routes/leaderboard/getLeaderboardGames.ts
@@ -1,8 +1,8 @@
 import { Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import { getLeaderboardGames } from "../../db/db.ts";
+import { getLeaderboardGamesAll } from "../../db/db.ts";
 
 export const getLeaderboardGames = async (ctx: Context) => {
-  const leaderboardGames = await getLeaderboardGames();
+  const leaderboardGames = await getLeaderboardGamesAll();
   const leaderboardGamesSorted = leaderboardGames.sort((a, b) =>
     b.score - a.score
   );
