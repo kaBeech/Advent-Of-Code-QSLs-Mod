938ec8206cfd3331702d8100bf0f0e72969e9955
kaBeech <contact@kaBeech.com>
2024-07-31 16:25:46 -0700

    Modularize User queries

--- backend_web/db/db.ts
+++ backend_web/db/db.ts
@@ -1,348 +1,77 @@
-import { Day, Game, User } from "../generated/client/deno/edge.ts";
-import { getAllChallengeModifierNames, getAllChallengeModifiers, getChallengeModifierDataById } from "./challengeModifiers/challengeModifier.ts";
-import { createDay, getDayById, getDayByUserIdGameNumberAndDayNumber, getDayIdByGameIdAndDayNumber, getPublicDayByGameIdAndNumber, updateDay } from "./days/day.ts";
-import { createGame, deleteGameById, deleteGamesByTesterId, getGameById, getGameByUserIdAndGameNumber, getGameDataByUserIdAndGameNumber, getGamesByUserId, getLeaderboardGamesQuery, getPublicGameById, getPublicGameSimpleById, updateGame } from "./games/games.ts";
-import { getAllModifierOptions, getModifierOptionDataById, getModifierOptionsByChallengeModifierId } from "./modifierOptions/modifierOption.ts";
-import { prisma } from "./prisma.ts";
-import { getAllTitles } from "./titles/title.ts";
+import {
+  createUser,
+  deleteUser,
+  getUserById,
+  getUserBySerializedId,
+  getUserByUsername,
+  getUserDataSimpleById,
+  getUserGameDataById,
+  getUserGameDayDataByIdGameNumberAndDayNumber,
+  getUserGamesListById,
+  getUserIdById,
+  getUserNumberOfGamesById,
+  updateUser,
+  updateUserName,
+  updateUserOAuthInfo,
+  upsertUser
+} from "./users/user.ts";
 
-/**
- * User CRUD
- */
+import {
+  createGame,
+  deleteGameById,
+  deleteGamesByTesterId,
+  getGameById,
+  getGameByUserIdAndGameNumber,
+  getGameDataByUserIdAndGameNumber,
+  getGamesByUserId,
+  getLeaderboardGamesQuery,
+  getPublicGameById,
+  getPublicGameSimpleById,
+  updateGame
+} from "./games/games.ts";
 
-export async function createUser(
-  id: string,
-  username?: string,
-) {
-  const result = await prisma.user.create({
-    data: {
-      id,
-      username,
-    },
-  });
-  return result;
-}
+import {
+  createDay,
+  getDayById,
+  getDayByUserIdGameNumberAndDayNumber,
+  getDayIdByGameIdAndDayNumber,
+  getPublicDayByGameIdAndNumber,
+  updateDay
+} from "./days/day.ts";
 
-export async function upsertUser(
-  id: string,
-  username?: string,
-  serializedId?: string,
-) {
-  const result = await prisma.user.upsert({
-    where: {
-      id,
-    },
-    update: {
-      username,
-      serializedId,
-    },
-    create: {
-      id,
-      username,
-      serializedId,
-    },
-  });
-  return result;
-}
+import {
+  getAllChallengeModifierNames,
+  getAllChallengeModifiers,
+  getChallengeModifierDataById
+} from "./challengeModifiers/challengeModifier.ts";
 
-export async function getUserByUsername(username: string) {
-  const user = await prisma.user.findUniqueOrThrow({
-    where: {
-      username,
-    },
-  });
-  return user;
-}
+import {
+  getAllModifierOptions,
+  getModifierOptionDataById,
+  getModifierOptionsByChallengeModifierId
+} from "./modifierOptions/modifierOption.ts";
 
-export async function getUserIdById(id: string) {
-  const user = await prisma.user.findUniqueOrThrow({
-    select: {
-      id: true,
-    },
-    where: {
-      id,
-    },
-  });
-  return user;
-}
-
-export async function getUserById(id: string) {
-  const user = await prisma.user.findUniqueOrThrow({
-    where: {
-      id,
-    },
-  });
-  return user;
-}
-
-export async function getUserBySerializedId(serializedId: string) {
-  const user = await prisma.user.findUniqueOrThrow({
-    where: {
-      serializedId,
-    },
-  });
-  return user;
-}
-
-export async function getUserDataSimpleById(
-  userId: string,
-) {
-  const user = await prisma.user.findUniqueOrThrow({
-    select: {
-      username: true,
-      oauthUsername: true,
-      oauthName: true,
-    },
-    where: {
-      id: userId,
-    },
-  });
-  return user;
-}
-
-export async function getUserNumberOfGamesById(
-  userId: string,
-) {
-  const user = await prisma.user.findUniqueOrThrow({
-    select: {
-      numberOfGames: true,
-      Game: {
-        select: {
-          number: true,
-        },
-      },
-    },
-    where: {
-      id: userId,
-    },
-  });
-  return user;
-}
-
-export async function getUserGamesListById(
-  userId: string,
-) {
-  const user = await prisma.user.findUniqueOrThrow({
-    select: {
-      numberOfGames: true,
-      Game: {
-        select: {
-          name: true,
-          number: true,
-          year: true,
-        },
-        orderBy: [
-          {
-            number: "asc",
-          },
-        ],
-      },
-    },
-    where: {
-      id: userId,
-    },
-  });
-  return user;
-}
-
-export async function getUserGameDataById(
-  userId: string,
-) {
-  const user = await prisma.user.findUniqueOrThrow({
-    select: {
-      username: true,
-      oauthAvatarUrl: true,
-      Game: {
-        select: {
-          year: true,
-          name: true,
-          id: true,
-          isPublic: true,
-          repositoryLink: true,
-          currentRerollTokens: true,
-          currentDay: true,
-          currentDayCompleted: true,
-          Day: {
-            select: {
-              modifierWhenPart1CompletedId: true,
-              challengeModifierId: true,
-              number: true,
-              challengeModifierRerollsUsed: true,
-              modifierOptionRerollsUsed: true,
-              rerollTokensSpentDuringPart2: true,
-              score: true,
-              part1Completed: true,
-              part2Completed: true,
-              dateFirstRolled: true,
-              ChallengeModifier: {
-                select: {
-                  text: true,
-                  explanatoryUrl: true,
-                },
-              },
-              ModifierOption: {
-                select: {
-                  text: true,
-                  explanatoryUrl: true,
-                },
-              },
-              ModifierWhenPart1Completed: {
-                select: {
-                  text: true,
-                  explanatoryUrl: true,
-                },
-              },
-              OptionWhenPart1Completed: {
-                select: {
-                  text: true,
-                  explanatoryUrl: true,
-                },
-              },
-            },
-          },
-        },
-      },
-    },
-    where: {
-      id: userId,
-    },
-  });
-  return user;
-}
-
-export async function getUserGameDayDataByIdGameNumberAndDayNumber(
-  userId: string,
-  gameNumber: number,
-  dayNumber: number,
-) {
-  const user = await prisma.user.findUniqueOrThrow({
-    select: {
-      username: true,
-      oauthAvatarUrl: true,
-    },
-    where: {
-      id: userId,
-    },
-  });
-  const game = await prisma.game.findFirstOrThrow({
-    select: {
-      year: true,
-      name: true,
-      id: true,
-      isPublic: true,
-      repositoryLink: true,
-      currentRerollTokens: true,
-      currentDay: true,
-      currentDayCompleted: true,
-    },
-    where: {
-      userId: userId,
-      number: gameNumber,
-    },
-  });
-  const day = await prisma.day.findFirstOrThrow({
-    select: {
-      modifierWhenPart1CompletedId: true,
-      challengeModifierId: true,
-      modifierOptionId: true,
-      number: true,
-      challengeModifierRerollsUsed: true,
-      modifierOptionRerollsUsed: true,
-      rerollTokensSpentDuringPart2: true,
-      score: true,
-      part1Completed: true,
-      part2Completed: true,
-      dateFirstRolled: true,
-      ChallengeModifier: {
-        select: {
-          text: true,
-          explanatoryUrl: true,
-        },
-      },
-      ModifierOption: {
-        select: {
-          text: true,
-          explanatoryUrl: true,
-        },
-      },
-      ModifierWhenPart1Completed: {
-        select: {
-          text: true,
-          explanatoryUrl: true,
-        },
-      },
-      OptionWhenPart1Completed: {
-        select: {
-          text: true,
-          explanatoryUrl: true,
-        },
-      },
-    },
-    where: {
-      userId: userId,
-      gameNumber: gameNumber,
-      number: dayNumber,
-    },
-  });
-  const userGameDayData = { user, game, day };
-  return userGameDayData;
-}
-
-export async function updateUser(user: User) {
-  const result = await prisma.user.update({
-    where: {
-      id: user.id,
-    },
-    data: {
-      numberOfGames: user.numberOfGames,
-    },
-  });
-  return result;
-}
-
-export async function updateUserName(id: string, username: string) {
-  const result = await prisma.user.update({
-    where: {
-      id,
-    },
-    data: {
-      username,
-    },
-  });
-  return result;
-}
-
-export async function updateUserOAuthInfo(
-  id: string,
-  oauthUrl: string,
-  oauthUsername: string,
-  oauthAvatarUrl: string,
-  oauthName?: string,
-) {
-  const result = await prisma.user.update({
-    where: {
-      id,
-    },
-    data: {
-      oauthUrl,
-      oauthUsername,
-      oauthAvatarUrl,
-      oauthName,
-    },
-  });
-  return result;
-}
-
-export async function deleteUser(id: string) {
-  const result = await prisma.user.delete({
-    where: {
-      id,
-    },
-  });
-  return result;
-}
+import {
+  getAllTitles
+} from "./titles/title.ts";
 
 export {
+  // User CRUD
+  createUser,
+  upsertUser,
+  getUserByUsername,
+  getUserIdById,
+  getUserById,
+  getUserBySerializedId,
+  getUserDataSimpleById,
+  getUserNumberOfGamesById,
+  getUserGamesListById,
+  getUserGameDataById,
+  getUserGameDayDataByIdGameNumberAndDayNumber,
+  updateUser,
+  updateUserName,
+  updateUserOAuthInfo,
+  deleteUser,
 
   // Game CRUD
   createGame,
