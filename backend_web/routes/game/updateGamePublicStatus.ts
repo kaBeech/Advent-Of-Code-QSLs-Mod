8839c4697653fca83179894996dfbf070651b837
kaBeech <contact@kaBeech.com>
2024-07-30 18:03:08 -0700

    Tune updateGamePublicStatus

--- backend_web/routes/game/updateGamePublicStatus.ts
+++ backend_web/routes/game/updateGamePublicStatus.ts
@@ -1,6 +1,6 @@
 import { State } from "https://deno.land/x/oak@v12.6.1/application.ts";
 import { RouterContext } from "https://deno.land/x/oak@v12.6.1/router.ts";
-import { getGamesByUserId, updateGame } from "../../db.ts";
+import { getGameByUserIdAndGameNumber, updateGame } from "../../db.ts";
 import { GameController } from "../../components/GameController.ts";
 
 export const updateGamePublicStatus = async (
@@ -17,8 +17,7 @@ export const updateGamePublicStatus = async (
   const isPublic = bodyData.fields.isPublic;
   const { gameNumber } = ctx.params;
   const userId = ctx.state.session.get("userId") as string;
-  const games = await getGamesByUserId(userId);
-  const game = games.find((game) => game.number === +gameNumber);
+  const game = await getGameByUserIdAndGameNumber(userId, +gameNumber);
   const updatedGame = GameController(game!).setPublicStatus(
     isPublic == "true" ? true : false,
   );
