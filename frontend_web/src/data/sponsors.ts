c47c91f53552c7172468bc7c5b2a97c9da664347
kaBeech <contact@kaBeech.com>
2024-07-31 19:25:33 -0700

    Update sponsor info

--- frontend_web/src/data/sponsors.ts
+++ frontend_web/src/data/sponsors.ts
@@ -4,22 +4,19 @@ export default [
     url: `/support`,
     textLine1: `I'm the creator of Xtreme Xmas
         Code; I hope you like it!`,
-    textLine2:
-      `Feel free to reach out with any questions/comments or if you'd like to
+    textLine2: `Feel free to reach out with any questions/comments or if you'd like to
         work together!`,
   },
   {
-    name: `kaBeech.com`,
-    url: `https://www.kabeech.com/`,
-    textLine1:
-      `kaBeech.com is also made by me (Kyle, the creator of Xtreme Xmas Code).`,
-    textLine2: `Check it out if you want to see some more cool stuff I made!`,
+    name: `FarReachCo.com`,
+    url: `https://www.farreachco.com/`,
+    textLine1: `Ready for a new millenium of roleplaying games?`,
+    textLine2: `Visit Far Reach Co. for original D&D content, remote tools, and more!`,
   },
   {
     name: `Thomas Johnston`,
     url: `https://thomasjohnston.services/`,
-    textLine1:
-      `I'm a software consultant specializing in full-stack web development and DevOps.`,
+    textLine1: `I'm a software consultant specializing in full-stack web development and DevOps.`,
     textLine2: `Can I help you build something?`,
   },
 ];
